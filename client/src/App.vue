ab0fd8bffdb108524c39f27038a78447c05c8e27
mholwill <m_holwill@hotmail.co.uk>
2020-03-06 16:46:15 +0000

    working on random function

--- client/src/App.vue
+++ client/src/App.vue
@@ -14,7 +14,8 @@ export default {
     return {
       categories: {},
       selectedCategory: null,
-      questions: {}
+      questions: {},
+      answers: []
     }
   },
   mounted(){
@@ -27,11 +28,59 @@ export default {
 
     fetch(`https://opentdb.com/api.php?amount=10&category=${this.selectedCategory.id}&difficulty=${this.selectedCategory.difficulty}&type=multiple`)
     .then(res => res.json())
-    .then(questions => this.questions = questions.results)
+    .then((questions) => {
+      this.questions = questions.results;
+      this.formattedQuestions(questions.results);
     })
+    this.shuffle(this.answers);
+  })
   },
+
+
   components: {
     'start-form': StartForm
+  },
+  methods: {
+    formattedQuestions: function (questions) {
+      questions.forEach((question) => {
+       let newArray = []
+       newArray.push(question.correct_answer)
+       newArray.push(question.incorrect_answers[0])
+       newArray.push(question.incorrect_answers[1])
+       newArray.push(question.incorrect_answers[2])
+       this.answers.push(newArray)
+     })
+   },
+
+   // randomiseAnswers: function(answers) {
+   //   let currentIndex = answers.length, temporaryValue, randomIndex;
+   //   while (0!==currentIndex){
+   //     randomIndex = Math.floor(Math.random() * currentIndex);
+   //     currentIndex -= 1;
+   //
+   //     temporaryValue = answers[currentIndex];
+   //     answers[currentIndex] = answers[randomIndex];
+   //     answers[randomIndex] = temporaryValue;
+   //   }
+   //   return answers;
+   //   console.log(randomiseAnswers(this.answers));
+   // }
+
+    shuffle: function(array) {
+      let ctr = array.length, temp, index;
+  // While there are elements in the array
+      while (ctr > 0) {
+  // Pick a random index
+          index = Math.floor(Math.random() * ctr);
+  // Decrease ctr by 1
+          ctr--;
+  // And swap the last element with it
+          temp = array[ctr];
+          array[ctr] = array[index];
+          array[index] = temp;
+      }
+      return array;
+    }
   }
 }
 </script>
