4969c5799aca5e927c799beb69cd39e758ec564b
mholwill <m_holwill@hotmail.co.uk>
2020-03-11 14:42:24 +0000

    save-form CSS complete

--- client/src/components/EndScoreForm.vue
+++ client/src/components/EndScoreForm.vue
@@ -1,23 +1,27 @@
 <template lang="html">
   <div class="container">
-    <form id="endscoreform" v-on:submit="handleSubmit">
+    <form id="end-score-form" v-on:submit="handleSubmit">
       <div class="formWrap">
         <label for="name">Your name: </label>
+        <br>
         <input type="text" id="name" v-model="name" required/>
       </div>
       <div class="formWrap">
         <label for="category">Category: </label>
-        <input type="text" id="category" :value="questionsAsked[0].category"/>
+        <br>
+        <input disabled="disabled" type="text" id="category" :value="questionsAsked[0].category"/>
       </div>
       <div class="formWrap">
         <label for="score">Total Score: </label>
-        <input type="text" id="score" :value="total"/>
+        <br>
+        <input disabled="disabled" type="text" id="score" :value="total"/>
       </div>
       <div class="formWrap">
         <label for="difficulty">Difficulty: </label>
-        <input type="text" id="difficulty" :value="questionsAsked[0].difficulty"/>
+        <br>
+        <input disabled="disabled" type="text" id="difficulty" :value="questionsAsked[0].difficulty"/>
       </div>
-      <input type="submit" value="Save" id="save"/>
+      <input class="submit-button" type="submit" value="Save" id="save"/>
     </form>
   </div>
 </template>
@@ -41,13 +45,48 @@ export default {
         Difficulty: this.questionsAsked[0].difficulty,
         Category: this.questionsAsked[0].category
       };
-      eventBus.$emit('score-added', payload)
-      }
-
+      ScoreService.postScores(payload)
+      .then(scores => {
+        eventBus.$emit('score-added', scores);
+      })
+    }
   },
   props: ['questionsAsked', 'total']
 }
 </script>
 
 <style lang="css" scoped>
+
+.container {
+  display: flex;
+  justify-content: center;
+}
+
+.formWrap {
+  font-size: 1.5em;
+}
+
+.formWrap input{
+  font-size: 0.75em;
+  padding: 5px;
+  border-radius: 8px;
+  margin: 10px 0;
+  background-color: none;
+  text-transform: capitalize;
+}
+
+.submit-button {
+  margin-top: 20px;
+  background-color: white;
+  width: 100%;
+  border: 2px #1C3144 solid;
+  margin-left: auto;
+  margin-right: auto;
+  padding: 20px;
+  cursor: pointer;
+  color: black;
+  border-radius: 8px;
+  font-size: 1em;
+  font-family: 'Quicksand', sans-serif;
+}
 </style>
