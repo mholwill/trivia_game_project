d084b0960a2e19f0ac5698a4a35d7096df1f1552
mholwill <m_holwill@hotmail.co.uk>
2020-03-11 14:31:31 +0000

    topScores Table complete

--- client/src/components/TopScores.vue
+++ client/src/components/TopScores.vue
@@ -1,7 +1,7 @@
 <template lang="html">
-  <div>
+  <div id="topScore-container">
     <h1>TOP SCORES</h1>
-    <table>
+    <table id="table-container">
       <tr>
         <th>Name</th>
         <th>Category</th>
@@ -9,7 +9,7 @@
         <th>Score</th>
       </tr>
 
-      <tr v-for="scores in filterScores">
+      <tr class="top-three" v-for="scores in filterScores">
         <td>{{scores.Name}}</td>
         <td>{{scores.Category}}</td>
         <td>{{scores.Difficulty}}</td>
@@ -33,11 +33,49 @@ export default {
         return -1;
         return 0;
       }
-      return this.topScores.sort(compare).slice(0, 15)
+      return this.topScores.sort(compare).slice(0, 10)
     }
   }
 }
 </script>
 
 <style lang="css" scoped>
+
+#topScore-container h1{
+  text-align: center;
+  font-size: 3em;
+  margin-top: 60px;
+  margin-bottom: 40px;
+}
+
+#table-container {
+  display: table;
+  text-align: center;
+  width: 65%;
+  margin: 0 auto;
+  border: 1px solid black;
+  border-collapse: collapse;
+}
+
+th {
+  padding: 7px 10px;
+  text-align: left;
+  font-weight: bolder;
+  border: 2px solid black;
+  font-size: 1em;
+  background-color: #0d71c5;
+  color: white;
+}
+
+td {
+  padding: 7px 10px;
+  text-align: left;
+  border: 1px solid black;
+  text-transform: capitalize;
+}
+
+.top-three:nth-last-of-type(1n+8) {
+  background-color: #fbd980;
+}
+
 </style>
