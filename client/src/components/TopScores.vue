0e17ca1223bf1c14374ac5a5276bb59ae20a711c
howmehow <howmehow@gmail.com>
2020-03-11 12:08:34 +0000

    topScores board sorted by best score

--- client/src/components/TopScores.vue
+++ client/src/components/TopScores.vue
@@ -1,12 +1,21 @@
 <template lang="html">
   <div>
     <h1>TOP SCORES</h1>
-    <ul v-for="scores in topScores">
-      <li>Name: {{scores.Name}}<br>
-        Difficulty: {{scores.Difficulty}}<br>
-        Score: {{scores.Score}}<br>
-        Category: {{scores.Category}}</li>
-    </ul>
+    <table>
+      <tr>
+        <th>Name</th>
+        <th>Category</th>
+        <th>Difficulty</th>
+        <th>Score</th>
+      </tr>
+
+      <tr v-for="scores in filterScores">
+        <td>{{scores.Name}}</td>
+        <td>{{scores.Category}}</td>
+        <td>{{scores.Difficulty}}</td>
+        <td>{{scores.Score}}</td>
+      </tr>
+    </table>
   </div>
 </template>
 
@@ -14,15 +23,21 @@
 
 export default {
   name: 'top-scores',
-  props: ['topScores']
-
+  props: ['topScores'],
+  computed: {
+    filterScores: function (){
+      function compare(a, b) {
+        if (a.Score < b.Score)
+        return 1;
+        if (a.Score > b.Score)
+        return -1;
+        return 0;
+      }
+      return this.topScores.sort(compare)
+    }
+  }
 }
 </script>
 
 <style lang="css" scoped>
 </style>
-
-Name: "John Harper",
-Score: 6,
-Difficulty: "medium",
-Category: "Animals"
